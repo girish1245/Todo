d21fb1ea907073fe0f581c397e457f5c459d63fc
girish1245 <girish1245@gmail.com>
2017-05-17 00:19:33 -0500

    Added remove task, selected class, stike text (with a bug)

--- Core/js/es5-script.js
+++ Core/js/es5-script.js
@@ -26,8 +26,11 @@ function addTask() {
         div.className = 'insideContent';
         var content = document.createTextNode(task);
         div.appendChild(content);
-        div.onmouseover = function () {
-            removeTask(e);
+        //     div.onmouseover = function() {
+        //         removeTask(event);
+        //    };
+        div.onclick = function () {
+            selected(event);
         };
         var div2 = document.createElement('div');
         div2.className = 'border';
@@ -43,8 +46,11 @@ function addTask() {
         var _content = document.createTextNode(task);
         _div.appendChild(_content);
         //        div.addEventListener("mouseover", removeTask(task), false);
-        _div.onmouseover = function () {
-            removeTask(e);
+        //  div.onmouseover = function() {
+        //     removeTask(event);
+        //  };
+        _div.onclick = function () {
+            selected(event);
         };
         var _div2 = document.createElement('div');
         _div2.className = 'border';
@@ -56,8 +62,28 @@ function addTask() {
 }
 
 function removeTask(cont) {
+    cont.path[1].removeChild(cont.path[0]);
+    //console.log(cont);
+};
 
-    console.log(cont);
+function selected(event) {
+
+    if (event.srcElement.localName === 'div' && event.offsetX > 450) {
+        removeTask(event);
+        event.path[1].removeChild(event.path[1].lastChild);
+    }
+
+    if (event.srcElement.localName === 'del' && event.offsetX < 450) {
+        event.path[1].innerHTML = event.path[0].innerText;
+        event.path[1].classList.remove('selected');
+        event.path[1].className = 'insideContent';
+    }
+
+    if (event.srcElement.localName !== 'del') {
+        event.path[0].className = 'selected';
+        event.path[0].innerHTML = '<del>' + event.path[0].innerHTML + '</del>';
+    }
+    console.log(event);
 };
 
 function removeAll() {
--- Core/js/es6-script.js
+++ Core/js/es6-script.js
@@ -26,8 +26,11 @@ function addTask() {
         div.className = 'insideContent';
         let content = document.createTextNode(task);
         div.appendChild(content);
-        div.onmouseover = function() {
-            removeTask(e);
+   //     div.onmouseover = function() {
+   //         removeTask(event);
+    //    };
+        div.onclick = function() {
+            selected(event);
         };
         let div2 = document.createElement('div');
         div2.className = 'border';
@@ -44,8 +47,11 @@ function addTask() {
         let content = document.createTextNode(task);
         div.appendChild(content);
 //        div.addEventListener("mouseover", removeTask(task), false);
-        div.onmouseover = function() {
-            removeTask(e);
+      //  div.onmouseover = function() {
+       //     removeTask(event);
+      //  };
+        div.onclick = function() {
+            selected(event);
         };
         let div2 = document.createElement('div');
         div2.className = 'border';
@@ -57,9 +63,30 @@ function addTask() {
 }
 
 function removeTask(cont) {
+    cont.path[1].removeChild(cont.path[0]);
+    //console.log(cont);
+};
 
-        console.log(cont);
+function selected(event) {
 
+   if(event.srcElement.localName === 'div' && (event.offsetX > 450)) {
+        removeTask(event);
+        event.path[1].removeChild(event.path[1].lastChild);
+
+   }
+
+    if(event.srcElement.localName === 'del' &&  (event.offsetX < 450)) {
+        event.path[1].innerHTML = event.path[0].innerText;
+        event.path[1].classList.remove('selected');
+        event.path[1].className = 'insideContent';
+
+    }
+
+    if(event.srcElement.localName !== 'del') {
+        event.path[0].className = 'selected';
+        event.path[0].innerHTML = '<del>' + event.path[0].innerHTML + '</del>'
+    }
+    console.log(event);
 };
 
 
