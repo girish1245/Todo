9d5844dde03410894f85ad71e7e205e33415b7ef
girish1245 <girish1245@gmail.com>
2017-05-16 22:26:06 -0500

    Added OnmouseOver, add task, remove task functions.

--- Core/js/es5-script.js
+++ Core/js/es5-script.js
@@ -1,5 +1,65 @@
-var k = function(v){
-    return v *423;
+'use strict';
+
+/**
+ * Created by Happy on 5/14/2017.
+ */
+
+var clearAll = function clearAll() {
+    var options = {};
+    $("#effect").effect('fold', options, 100, removeAll());
 };
 
-k(343);
+/*$("#button").on( "click", function() {
+    clearAll();
+    return false;
+});
+*/
+
+function addTask() {
+    var task = document.getElementById('tasks').value;
+    //alert(task);
+    if (task === '') {
+        return;
+    }
+    if (document.getElementById('effect') !== null) {
+        var div = document.createElement('div');
+        div.className = 'insideContent';
+        var content = document.createTextNode(task);
+        div.appendChild(content);
+        div.onmouseover = function () {
+            removeTask(e);
+        };
+        var div2 = document.createElement('div');
+        div2.className = 'border';
+        document.getElementById('effect').appendChild(div);
+        document.getElementById('effect').appendChild(div2);
+    } else {
+        var parentDiv = document.createElement('div');
+        parentDiv.className = 'contents ui-widget-header ui-corner-all';
+        parentDiv.setAttribute('id', 'effect');
+        document.getElementsByTagName('body')[0].appendChild(parentDiv);
+        var _div = document.createElement('div');
+        _div.className = 'insideContent';
+        var _content = document.createTextNode(task);
+        _div.appendChild(_content);
+        //        div.addEventListener("mouseover", removeTask(task), false);
+        _div.onmouseover = function () {
+            removeTask(e);
+        };
+        var _div2 = document.createElement('div');
+        _div2.className = 'border';
+        document.getElementById('effect').appendChild(_div);
+        document.getElementById('effect').appendChild(_div2);
+    }
+
+    //'<div id="effect" class="insideContent">' + task + '</div><div class="border"></div>';
+}
+
+function removeTask(cont) {
+
+    console.log(cont);
+};
+
+function removeAll() {
+    document.getElementById('effect').remove();
+}
--- Core/js/es6-script.js
+++ Core/js/es6-script.js
@@ -1,3 +1,68 @@
 /**
  * Created by Happy on 5/14/2017.
  */
+
+
+var clearAll = () => {
+    let options = {};
+    $("#effect").effect( 'fold', options, 100, removeAll());
+};
+
+/*$("#button").on( "click", function() {
+    clearAll();
+    return false;
+});
+*/
+
+
+function addTask() {
+    const task = document.getElementById('tasks').value;
+    //alert(task);
+    if (task === '') {
+        return;
+    }
+    if (document.getElementById('effect') !== null) {
+        let div = document.createElement('div');
+        div.className = 'insideContent';
+        let content = document.createTextNode(task);
+        div.appendChild(content);
+        div.onmouseover = function() {
+            removeTask(e);
+        };
+        let div2 = document.createElement('div');
+        div2.className = 'border';
+        document.getElementById('effect').appendChild(div);
+        document.getElementById('effect').appendChild(div2);
+    }
+    else {
+        let parentDiv = document.createElement('div');
+        parentDiv.className = 'contents ui-widget-header ui-corner-all';
+        parentDiv.setAttribute('id', 'effect');
+        document.getElementsByTagName('body')[0].appendChild(parentDiv);
+        let div = document.createElement('div');
+        div.className = 'insideContent';
+        let content = document.createTextNode(task);
+        div.appendChild(content);
+//        div.addEventListener("mouseover", removeTask(task), false);
+        div.onmouseover = function() {
+            removeTask(e);
+        };
+        let div2 = document.createElement('div');
+        div2.className = 'border';
+        document.getElementById('effect').appendChild(div);
+        document.getElementById('effect').appendChild(div2);
+    }
+
+        //'<div id="effect" class="insideContent">' + task + '</div><div class="border"></div>';
+}
+
+function removeTask(cont) {
+
+        console.log(cont);
+
+};
+
+
+function removeAll() {
+    document.getElementById('effect').remove();
+}
